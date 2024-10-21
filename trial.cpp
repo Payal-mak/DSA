eb63e1ef1f38d2266cadb408ce9c18db73eac9a6
Payal-mak <iampayal018@gmail.com>
2024-10-21 16:16:59 +0530

    new change

--- trial.cpp
+++ trial.cpp
@@ -1,32 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int MAX_NODES = 10;
 bool visited[MAX_NODES];
 bool adjacenceyMatrix[MAX_NODES][MAX_NODES];
 
-int main(){
-    int total_nodes,edges;
+
+void BFS(int startnode, int total_nodes)
+{
+    queue<int> q;
+    q.push(startnode);
+    visited[startnode] = true;
+
+    while(!q.empty()){
+        int currentnode;
+        currentnode = q.front();
+        cout << currentnode << " ";
+        q.pop();
+
+        for(int i = 0; i < total_nodes; i++){
+            if(adjacenceyMatrix[currentnode][i] = true && !visited[i]){
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int total_nodes, edges;
     cout << "enter the number of nodes: ";
     cin >> total_nodes;
 
     cout << "enter the number of edges: ";
     cin >> edges;
 
-    //define the adjaceny matrix with false
-    for(int i = 0; i < total_nodes; i++){
-        for(int j = 0; j < total_nodes; j++){
+    // define the adjaceny matrix with false
+    for (int i = 0; i < total_nodes; i++)
+    {
+        for (int j = 0; j < total_nodes; j++)
+        {
             adjacenceyMatrix[i][j] = false;
         }
-    } 
+    }
 
     cout << "enter the edges (u,v)" << endl;
-    for(int i = 0; i < edges; i++){
-        int u,v;
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
         cin >> u >> v;
         adjacenceyMatrix[u][v] = true;
-        adjacenceyMatrix[v][u] = true; //for undirected graph only
-        
+        adjacenceyMatrix[v][u] = true; // for undirected graph only
+
     }
+    int startnode;
+    cout << "the starting node of BFS: ";
+    cin >> startnode;
+
+    BFS(startnode, total_nodes);
     return 0;
 }
